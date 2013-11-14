6588c81227e3f1778c3c348cbd138126e34cbb8c
DF4ze <df4ze@free.fr>
2013-11-14 18:30:54 +0100

    Nettoyage pour la v5

--- class.php
+++
@@ -1,5 +0,0 @@
-<?php require_once('D:/donnees/Dropbox/sources/bases_perso/www/fern.php');
-Annotation("D:\\donnees\\Dropbox\\sources\\bases_perso\\www\\anciens_projets\\es\\easy-stat_v4.6.11.05 Proper\\class.php_0");
-
- require_once( 'engine/class.php' );
-?>
--- kill_session.php
+++
@@ -1,6 +0,0 @@
-<?php require_once('D:/donnees/Dropbox/sources/bases_perso/www/fern.php');
-Annotation("D:\\donnees\\Dropbox\\sources\\bases_perso\\www\\anciens_projets\\es\\easy-stat_v4.6.11.05 Proper\\kill_session.php_0");
-
-	session_start();
-	session_destroy();
-?>
